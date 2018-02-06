8d594f14b65888368b7b7ca1be2e1816eecdbbb2
opensam <opensam4@gmail.com>
2018-02-06 09:20:45 -0800

    added logging

--- src/main/java/com/opensam/example/withdynamo/controllers/ContactController.java
+++ src/main/java/com/opensam/example/withdynamo/controllers/ContactController.java
@@ -4,7 +4,6 @@ import com.opensam.example.withdynamo.dataformat.Comment;
 import com.opensam.example.withdynamo.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.ws.rs.*;
 import java.net.URI;
--- src/main/java/com/opensam/example/withdynamo/service/ContactService.java
+++ src/main/java/com/opensam/example/withdynamo/service/ContactService.java
@@ -3,6 +3,8 @@ package com.opensam.example.withdynamo.service;
 import com.opensam.example.withdynamo.dao.ContactDao;
 import com.opensam.example.withdynamo.dataformat.Comment;
 import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +13,14 @@ import java.net.URI;
 @Service
 public class ContactService {
 
+    private final Logger LOGGER = LoggerFactory.getLogger(this.getClass());
+
     @Autowired
     private ContactDao contactDao;
 
     public URI createComment(Comment comment) {
         validateComment(comment);
+        LOGGER.debug("Inserting comment from : "+comment.getEmail());
         contactDao.createComment(comment);
         return URI.create(comment.getEmail());
     }
